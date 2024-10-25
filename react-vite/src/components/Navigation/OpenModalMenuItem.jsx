5d2112affe08b94c868361e72fd66e8c881614cd
nhatdngo <nhatdngo@gmail.com>
2024-10-24 23:15:46 -0700

    deleted some stuff

--- react-vite/src/components/Navigation/OpenModalMenuItem.jsx
+++ react-vite/src/components/Navigation/OpenModalMenuItem.jsx
@@ -1,17 +1,17 @@
 import { useModal } from '../../context/Modal';
 
 function OpenModalMenuItem({
-  modalComponent, // component to render inside the modal
-  itemText, // text of the button that opens the modal
-  onItemClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
+  modalComponent,
+  itemText,
+  onItemClick,
+  onModalClose
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
   const onClick = () => {
     if (onModalClose) setOnModalClose(onModalClose);
     setModalContent(modalComponent);
-    if (typeof onItemClick === "function") onItemClick();
+    if (typeof onItemClick === "function") onItemClick()
   };
 
   return (
